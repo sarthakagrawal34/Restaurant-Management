4dab12b0d4a866a88af55b062e326d2e8f6c0e8a
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2024-06-11 11:10:41 +0530

    establish database connection

--- database/mongoConnection.go
+++ database/mongoConnection.go
@@ -1,1 +1,41 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoClient *mongo.Client = DBinstance()
+
+func DBinstance() *mongo.Client {
+	fmt.Println("MongoDB connection in progress...")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading the .env file")
+	}
+
+	MongoDB := os.Getenv("MONGODB_URL")
+	// Set up a context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	clientOptions := options.Client().ApplyURI(MongoDB)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	fmt.Println("MongoDB is connected successfully")
+	return client
+}
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+	return collection
+}
